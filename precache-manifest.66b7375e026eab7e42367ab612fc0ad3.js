fea93dd0d547acaaf90d15fade991dad3fa96ab1
kyaw-lwin <kyawlwin388@gmail.com>
2022-07-01 10:03:02 +0100

    Updates

---
+++ precache-manifest.66b7375e026eab7e42367ab612fc0ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ac75701b4344ed0a482398643e598a",
+    "url": "/EventApp-Front-End/index.html"
+  },
+  {
+    "revision": "0062c00fba98e0854543",
+    "url": "/EventApp-Front-End/static/css/main.acbf3200.chunk.css"
+  },
+  {
+    "revision": "1617906d5ff90b757034",
+    "url": "/EventApp-Front-End/static/js/2.77f62344.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/EventApp-Front-End/static/js/2.77f62344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0062c00fba98e0854543",
+    "url": "/EventApp-Front-End/static/js/main.5652eef7.chunk.js"
+  },
+  {
+    "revision": "53545f938ed493077f99",
+    "url": "/EventApp-Front-End/static/js/runtime-main.b454cfae.js"
+  },
+  {
+    "revision": "33ef4db5db435395636402565a3018ba",
+    "url": "/EventApp-Front-End/static/media/event image 1.33ef4db5.jpg"
+  }
+]);
